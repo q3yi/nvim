6e1f54d437980b4e2225ab4ddd5e117cd54d2a5b
Yi Qing <qingyi.tss@gmail.com>
2023-11-23 15:24:28 +0800

    remove doom-theme.

--- lua/q3yi/theme.lua
+++ lua/q3yi/theme.lua
@@ -1,30 +1,5 @@
 -- Set color theme and status line
 
--- local DoomOneTheme = {
---     "NTBBloodbath/doom-one.nvim",
---     config = function()
---         vim.g.doom_one_terminal_colors = true
---         vim.g.doom_one_italic_comments = true
---         vim.g.doom_one_enable_treesitter = true
---         -- Color whole diagnostic text or only underline
---         vim.g.doom_one_diagnostics_text_color = false
---         -- Enable transparent background
---         vim.g.doom_one_transparent_background = false
---
---         -- Pumblend transparency
---         vim.g.doom_one_pumblend_enable = false
---         vim.g.doom_one_pumblend_transparency = 20
---
---         vim.g.doom_one_plugin_telescope = true
---         vim.g.doom_one_plugin_neogit = true
---         vim.g.doom_one_plugin_nvim_tree = true
---         vim.g.doom_one_plugin_whichkey = true
---         vim.g.doom_one_plugin_indent_blankline = true
---
---         -- vim.cmd [[colorscheme doom-one]]
---     end
--- }
-
 local CatppuccinTheme = {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -35,6 +10,10 @@ local CatppuccinTheme = {
             background = {
                 light = "latte",
                 dark = "mocha",
+            },
+            transparent_background = true,
+            styles = {
+                conditionals = {},
             },
             integrations = {
                 cmp = true,
@@ -69,8 +48,8 @@ local Lualine = {
             theme = "catppuccin",
             -- component_separators = "|",
             -- section_separators = "",
-            component_separators = { left = "\\", right = "/" },
-            section_separators = { left = '', right = '' },
+            -- component_separators = { left = "\\", right = "/" },
+            -- section_separators = { left = '', right = '' },
             -- disabled_filetypes = {
             --     statusline = { "NvimTree" },
             -- },
