9de51b2dc437321b668ad96e0a8a398e500b1e2d
Yi Qing <qingyi.tss@gmail.com>
2023-12-02 20:11:07 +0800

    use mason to install and config lsp servers.

--- lua/q3yi/lsp.lua
+++ lua/q3yi/lsp.lua
@@ -1,25 +1,20 @@
 -- Configurate language server
 
 local M = {
-    -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
+    event = { "VeryLazy" },
     dependencies = {
-        -- Adds LSP completion capabilities
         "hrsh7th/cmp-nvim-lsp",
-
-        -- Useful status updates for LSP
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-        -- Additional lua configuration, makes nvim stuff amazing!
+        { 'j-hui/fidget.nvim', opts = {} },
         'folke/neodev.nvim',
+
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
     },
 }
 
 local function on_attach(_, buf)
-    -- In this case, we create a function that lets us more easily define mappings specific
-    -- for LSP related items. It sets the mode, buffer and description for us each time.
-    local kmap = function(keys, func, desc)
+    local function kmap(keys, func, desc)
         if desc then
             desc = "LSP: " .. desc
         end
@@ -27,18 +22,16 @@ local function on_attach(_, buf)
         vim.keymap.set("n", keys, func, { buffer = buf, desc = desc })
     end
 
-    local telescope = require("telescope.builtin")
-
     kmap("<leader>lr", vim.lsp.buf.rename, "Rename")
     kmap("<leader>la", vim.lsp.buf.code_action, "Code action")
 
-    kmap("gd", telescope.lsp_definitions, "Goto definition")
-    kmap("gr", telescope.lsp_references, "Goto references")
-    kmap("gI", telescope.lsp_implementations, "Goto implementation")
+    kmap("gd", "<cmd>Telescope lsp_definitions theme=dropdown<cr>", "Goto definition")
+    kmap("gr", "<cmd>Telescope lsp_references theme=dropdown<cr>", "Goto references")
+    kmap("gI", "<cmd>Telescope lsp_implementations theme=dropdown<cr>", "Goto implementation")
 
-    kmap("<leader>lD", telescope.lsp_type_definitions, "Type definition")
-    kmap("<leader>ls", telescope.lsp_document_symbols, "Buffer symbols")
-    kmap("<leader>lS", telescope.lsp_dynamic_workspace_symbols, "Workspace symbols")
+    kmap("<leader>lD", "<cmd>Telescope lsp_type_definitions theme=dropdown", "Type definition")
+    kmap("<leader>ls", "<cmd>Telescope lsp_document_symbols theme=dropdown", "Buffer symbols")
+    kmap("<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols", "Workspace symbols")
     kmap("<leader>lf", function() vim.lsp.buf.format { async = true } end, "Format current buffer")
 
     -- See `:help K` for why this keymap
@@ -52,38 +45,10 @@ local function on_attach(_, buf)
     kmap("<leader>wfl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, "List workspace folders")
+
+    kmap("<leader>li", "<cmd>LspInfo<cr>", "Show attached lsp server info")
+    kmap("<leader>lu", "<cmd>LspRestart<cr>", "Restart lsp server")
 end
-
-local servers = {
-    -- clangd = {},
-    gopls = {},
-    hls = {},
-    html = { filetypes = { "html", "twig", "hbs" } },
-    jsonls = {},
-    pylsp = {
-        -- TODO nvim-lsp not supported yet?
-        -- pylsp = {
-        --     plugins = {
-        --         rope_autoimport = { enabled = true },
-        --     },
-        -- },
-    },
-    rust_analyzer = {},
-    tsserver = {},
-    yamlls = {
-        -- TODO not working, why?
-        yaml = {
-            format = { enable = true },
-        },
-    },
-
-    lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    },
-}
 
 function M.config()
     require("neodev").setup({})
@@ -92,15 +57,42 @@ function M.config()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    local lspconfig = require("lspconfig")
+    require('lspconfig.ui.windows').default_options.border = "rounded"
 
-    for server, opt in pairs(servers) do
-        lspconfig[server].setup {
+    local function setup_lsp(server_name, settings)
+        require("lspconfig")[server_name].setup {
             capabilities = capabilities,
             on_attach = on_attach,
-            settings = opt,
+            settings = settings,
         }
     end
+
+    require("mason").setup({ ui = { border = "rounded" } })
+    local mason_lsp = require("mason-lspconfig")
+    mason_lsp.setup({})
+
+    -- Setup lspconfig automatically when install lsp server in mason
+    mason_lsp.setup_handlers {
+        -- default setting for server doesn't have a dedicated handler
+        function(server_name)
+            setup_lsp(server_name, {})
+        end,
+
+        -- override lsp server settings with a dedicated handler
+        ["lua_ls"] = function()
+            local opt = {
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                },
+            }
+            setup_lsp("lua_ls", opt)
+        end,
+        ["html"] = function()
+            local opt = { filetypes = { "html", "twig", "hbs" } }
+            setup_lsp("html", opt)
+        end
+    }
 end
 
 return M
