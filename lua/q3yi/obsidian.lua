28960bd93908e8943d247208f936d73d830e35b1
Qing Yi <qing.yi@outlook.com>
2024-04-19 16:07:32 +0800

    add `created_at` time when create note in obsidian.

--- lua/q3yi/obsidian.lua
+++ lua/q3yi/obsidian.lua
@@ -42,7 +42,37 @@ local M = {
                     end
                 end
                 return os.date("%Y%m%d%H%M", os.time()) .. "-" .. suffix
-            end
+            end,
+            note_frontmatter_func = function(note)
+                -- Add the title of the note as an alias.
+                if note.title then
+                    note:add_alias(note.title)
+                end
+
+                local out = {
+                    id = note.id,
+                    aliases = note.aliases,
+                    tags = note.tags,
+                }
+
+                -- `note.metadata` contains any manually added fields in the frontmatter.
+                -- So here we just make sure those fields are kept in the frontmatter.
+                if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                    for k, v in pairs(note.metadata) do
+                        out[k] = v
+                    end
+                end
+
+                if out.categories == nil then
+                    out.categories = {}
+                end
+
+                if out.created_at == nil then
+                    out.created_at = os.date("%Y-%m-%dT%H:%M:%S", os.time())
+                end
+
+                return out
+            end,
         })
     end,
 }
