e194b7d096c39343be70249b0fae46bff6504e0a
Qing Yi <qing.yi@outlook.com>
2024-06-29 20:01:53 +0800

    dont integrate fidget.nvim with nvim-tree.

--- lua/q3yi/lsp.lua
+++ lua/q3yi/lsp.lua
@@ -8,11 +8,10 @@ local M = {
         {
             "j-hui/fidget.nvim",
             opts = {
-                notification = {
-                    window = {
-                        winblend = 0,
-                        -- border = "single",
-                    },
+                notification = { window = { winblend = 0 } },
+                integration = {
+                    ["nvim-tree"] = { enable = false },
+                    ["xcodebuild-nvim"] = { enable = false },
                 },
             },
         },
