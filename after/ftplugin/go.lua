f0fbab643845d48da9e4b3b523d05fbcde1a682a
Qing Yi <qing.yi@outlook.com>
2024-07-01 22:56:07 +0800

    set tabwidth base on filetype.

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,2 @@
+-- set local config for go file
+vim.opt_local.expaandtab = false
---
+++ after/ftplugin/ocaml.lua
@@ -0,0 +1,6 @@
+-- set local config for ocaml file
+local opt = vim.opt_local
+
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
