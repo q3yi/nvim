0c5055a1a48140e81178b2aa033e467c1c531983
Qing Yi <qing.yi@outlook.com>
2025-02-14 13:50:08 +0800

    read DARK_MODE env instead of NVIM_THEME env to set colorscheme.

--- lua/q3yi/theme/nightfox.lua
+++ lua/q3yi/theme/nightfox.lua
@@ -21,7 +21,10 @@ local M = {
 
         require("nightfox").setup(opts)
 
-        local theme = os.getenv("NVIM_THEME") or "nightfox"
+        local theme = "nightfox"
+        if os.getenv("DARK_MODE") == "false" then
+            theme = "dayfox"
+        end
 
         vim.cmd.colorscheme(theme)
     end,
@@ -29,7 +32,7 @@ local M = {
         vim.keymap.set({ "n", "v" }, "<leader>utd", "<cmd>colorscheme dayfox<cr>", { desc = "dayfox" })
         vim.keymap.set({ "n", "v" }, "<leader>utn", "<cmd>colorscheme nightfox<cr>", { desc = "nightfox" })
         vim.keymap.set({ "n", "v" }, "<leader>utu", "<cmd>colorscheme duskfox<cr>", { desc = "duskfox" })
-        vim.keymap.set({ "n", "v" }, "<leader>utu", "<cmd>colorscheme dawnfox<cr>", { desc = "dawnfox" })
+        vim.keymap.set({ "n", "v" }, "<leader>utw", "<cmd>colorscheme dawnfox<cr>", { desc = "dawnfox" })
     end,
 }
 
