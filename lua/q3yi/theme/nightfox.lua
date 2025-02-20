a252ce12b204940efa49dcd806544ac9b7f0d47d
Qing Yi <qing.yi@outlook.com>
2025-02-20 14:43:44 +0800

    change theme base on vim.o.background.

--- lua/q3yi/theme/nightfox.lua
+++ lua/q3yi/theme/nightfox.lua
@@ -18,21 +18,24 @@ local M = {
         -- local acme = require("q3yi.theme.color.acme")
         -- opts = helix.apply(opts)
         -- opts = acme.apply(opts)
-
         require("nightfox").setup(opts)
-
-        local theme = "nightfox"
-        if os.getenv("DARK_MODE") == "false" then
-            theme = "dayfox"
-        end
-
-        vim.cmd.colorscheme(theme)
     end,
     init = function()
         vim.keymap.set({ "n", "v" }, "<leader>utd", "<cmd>colorscheme dayfox<cr>", { desc = "dayfox" })
         vim.keymap.set({ "n", "v" }, "<leader>utn", "<cmd>colorscheme nightfox<cr>", { desc = "nightfox" })
         vim.keymap.set({ "n", "v" }, "<leader>utu", "<cmd>colorscheme duskfox<cr>", { desc = "duskfox" })
         vim.keymap.set({ "n", "v" }, "<leader>utw", "<cmd>colorscheme dawnfox<cr>", { desc = "dawnfox" })
+
+        vim.api.nvim_create_autocmd("OptionSet", {
+            pattern = "background",
+            callback = function()
+                if vim.o.background == "light" then
+                    vim.cmd.colorscheme("dayfox")
+                else
+                    vim.cmd.colorscheme("nightfox")
+                end
+            end,
+        })
     end,
 }
 
