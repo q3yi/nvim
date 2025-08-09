95ce39a47478c6e7cb5ed862a317378044da12e7
Qing Yi <qing.yi@outlook.com>
2025-08-09 17:29:07 +0800

    enable diagnostic virtual lines.

--- lua/q3yi/diagnostic.lua
+++ lua/q3yi/diagnostic.lua
@@ -1,36 +1,36 @@
 -- Set diagnostic shortcuts
 
-local options = {
-    config = {
-        signs = {
-            text = {
-                [vim.diagnostic.severity.ERROR] = "‼",
-                [vim.diagnostic.severity.WARN] = "!",
-                [vim.diagnostic.severity.HINT] = "!",
-                [vim.diagnostic.severity.INFO] = "!",
-            },
+local opts = {
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "‼",
+            [vim.diagnostic.severity.WARN] = "!",
+            [vim.diagnostic.severity.HINT] = "!",
+            [vim.diagnostic.severity.INFO] = "!",
         },
-        virtual_text = function()
-            if vim.g.diagnostic_show_virtual_text == nil then
-                vim.g.diagnostic_show_virtual_text = true
-            end
-            return vim.g.diagnostic_show_virtual_text
-        end,
-        underline = true,
-        update_in_insert = true,
-        serverity_sort = true,
-        float = {
-            focusable = true,
-            source = "always",
-        },
+    },
+    virtual_text = function()
+        if vim.g.diagnostic_show_virtual_text == nil then
+            vim.g.diagnostic_show_virtual_text = true
+        end
+        return vim.g.diagnostic_show_virtual_text
+    end,
+    virtual_lines = function()
+        if vim.g.diagnostic_show_virtual_text then
+            return false
+        end
+
+        return { current_line = true }
+    end,
+    underline = true,
+    update_in_insert = true,
+    serverity_sort = true,
+    float = {
+        focusable = true,
+        source = "always",
     },
 }
 
-local function setup_diagnostic(opts)
-    vim.diagnostic.config(opts.config)
-    vim.keymap.set("n", "<leader>dk", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-    vim.keymap.set("n", "<leader>dq", vim.diagnostic.setqflist, { desc = "Set diagnostic to quickfix" })
-end
-
--- initial diagnostic config here.
-setup_diagnostic(options)
+vim.diagnostic.config(opts)
+vim.keymap.set("n", "<leader>dk", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>dq", vim.diagnostic.setqflist, { desc = "Set diagnostic to quickfix" })
