8d0fb94caef850a65c5b3d3f1d33ca28775b2bff
Qing Yi <qing.yi@outlook.com>
2025-12-10 21:13:21 +0800

    update lsp config, replace snacks with mini picker.

--- lua/q3yi/lsp.lua
+++ lua/q3yi/lsp.lua
@@ -3,43 +3,43 @@
 local M = {
     "neovim/nvim-lspconfig",
     event = { "VeryLazy" },
-    dependencies = {
-        "mason-org/mason.nvim",
-        "mason-org/mason-lspconfig.nvim",
-    },
     config = function()
-        require("mason").setup({ ui = { border = "single" } })
-        require("mason-lspconfig").setup()
-
-        -- Some lsp server should install with its own environment instead of install through Mason,
-        -- so we just enable it manually.
-        vim.lsp.enable({ "emmylua_ls", "pylsp", "ocamllsp", "gopls", "zls", "marksman" })
+        vim.lsp.enable({
+            "denojs",
+            "emmylua_ls",
+            "gopls",
+            "marksman",
+            "ocamllsp",
+            "pylsp",
+            "rust_analyzer",
+            "zls",
+        })
     end,
 }
 
 local function bind_keys(event)
     local buf = event.buf
+    local pickers = require("mini.extra").pickers
 
     local bindings = {
-        { "gd",          Snacks.picker.lsp_definitions,       "Goto definition" },
-        { "gD",          Snacks.picker.lsp_declarations,      "Goto declaration" },
-
-        { "grr",         Snacks.picker.lsp_references,        "Goto references" },
-        { "gri",         Snacks.picker.lsp_implementations,   "Goto implementation" },
-        { "grn",         vim.lsp.buf.rename,                  "Rename" },
-        { "gra",         vim.lsp.buf.code_action,             "Code action" },
-        { "grt",         Snacks.picker.lsp_type_definitions,  "Type definition" },
-
-        { "gO",          Snacks.picker.lsp_symbols,           "Buffer symbols" },
-        { "<leader>ws",  Snacks.picker.lsp_workspace_symbols, "Workspace symbols" },
+        { "gd",          vim.lsp.buf.definition,                                     "Goto definition" },
+        { "gD",          vim.lsp.buf.declaration,                                    "Goto declaration" },
+        { "grr",         function() pickers.lsp({ scope = "references" }) end,       "Goto references" },
+        { "gri",         function() pickers.lsp({ scope = "implementation" }) end,   "Goto implementation" },
+        { "grn",         vim.lsp.buf.rename,                                         "Rename" },
+        { "gra",         vim.lsp.buf.code_action,                                    "Code action" },
+        { "grt",         function() pickers.lsp({ scope = "type_definition" }) end,  "Type definition" },
+        --
+        { "gO",          function() pickers.lsp({ scope = "document_symbol" }) end,  "Buffer symbols" },
+        { "<leader>ws",  function() pickers.lsp({ scope = "workspace_symbol" }) end, "Workspace symbols" },
 
         -- See `:help K` for why this keymap
-        { "K",           vim.lsp.buf.hover,                   "Hover documentation" },
-        { "<M-K>",       vim.lsp.buf.signature_help,          "Signature documentation" },
+        { "K",           vim.lsp.buf.hover,                                          "Hover documentation" },
+        { "<M-K>",       vim.lsp.buf.signature_help,                                 "Signature documentation" },
 
         -- Lesser used LSP functionality
-        { "<leader>wfa", vim.lsp.buf.add_workspace_folder,    "Add folder to workspace" },
-        { "<leader>wfr", vim.lsp.buf.remove_workspace_folder, "Remove folder from workspace" },
+        { "<leader>wfa", vim.lsp.buf.add_workspace_folder,                           "Add folder to workspace" },
+        { "<leader>wfr", vim.lsp.buf.remove_workspace_folder,                        "Remove folder from workspace" },
         {
             "<leader>wfl",
             function()
@@ -50,11 +50,7 @@ local function bind_keys(event)
     }
 
     for _, binding in ipairs(bindings) do
-        local key, cmd, desc = binding[1], binding[2], binding[3]
-        if desc then
-            desc = "LSP: " .. desc
-        end
-
+        local key, cmd, desc = binding[1], binding[2], "LSP: " .. binding[3]
         vim.keymap.set("n", key, cmd, { buffer = buf, desc = desc })
     end
 end
