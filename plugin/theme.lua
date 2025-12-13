b8d2acc2210c23af72da596033d6d1214101ad1b
Qing Yi <qing.yi@outlook.com>
2025-12-13 16:17:36 +0800

    make comments italic.

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -1,13 +1,14 @@
 -- config colorscheme
 
+---@diagnostic disable-next-line: param-type-mismatch
 require("rose-pine").setup({
     variant = "auto",
     dark_variant = "main",
-    styles = {
-        -- bold = true,
-        italic = false,
-    },
+    styles = { italic = false },
     highlight_groups = {
+        Comment = { italic = true },
+        ["@markup.italic"] = { italic = true },
+
         DapBreak = { fg = "love", bg = "love", blend = 10 },
         DapStop = { fg = "gold", bg = "gold", blend = 10 },
     },
