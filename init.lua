1a5c5d1d1a6e4b7cc1135a018664a16d24af8244
Qing Yi <qing.yi@outlook.com>
2026-07-23 09:08:22 +0800

    feat: migrate theme from rose-pine/vague to ef-themes

--- init.lua
+++ init.lua
@@ -9,10 +9,10 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.pack.add({
-    { src = "https://github.com/nvim-mini/mini.nvim",                         version = "main" },
-    { src = "https://github.com/rose-pine/neovim",                            name = "rose-pine" },
-    { src = "https://github.com/vague-theme/vague.nvim" },
-    { src = "https://github.com/saghen/blink.cmp",                            version = vim.version.range("1.*") },
+    { src = "https://github.com/nvim-mini/mini.nvim", version = "main" },
+    { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+    { src = "https://github.com/oonamo/ef-themes.nvim" },
+    { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
     { src = "https://github.com/rafamadriz/friendly-snippets" },
     { src = "https://github.com/romus204/tree-sitter-manager.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = "main" },
--- plugin/theme.lua
+++ plugin/theme.lua
@@ -1,34 +1,16 @@
----@diagnostic disable-next-line: param-type-mismatch
-function setup_rose_pine()
-    require("rose-pine").setup({
-        variant = "auto",
-        dark_variant = "main",
-        styles = { italic = false },
-        highlight_groups = {
-            Normal = { bg = "none" },
-            NormalNC = { bg = "none" },
-            -- StatusLine = { fg = "love", bg = "love", blend = 10 },
-            -- StatusLineNC = { fg = "subtle", bg = "surface" },
+--- setup theme
 
-            Comment = { italic = true },
-            ["@markup.italic"] = { italic = true },
+local theme_opts = {
+    light = "ef-duo-light",
+    dark = "ef-duo-dark",
+    modules = {
+        mini = true,
+    },
+    options = {
+        compile = false, -- compile will hardcode vim.o.background, disable auto theme switch
+    },
+} ---@as Ef-Themes.Config
 
-            DapBreak = { fg = "love", bg = "love", blend = 10 },
-            DapStop = { fg = "gold", bg = "gold", blend = 10 },
-        },
-    })
-end
+require("ef-themes").setup(theme_opts)
 
-vim.api.nvim_create_autocmd("ColorScheme", {
-    group = vim.api.nvim_create_augroup("colorscheme.loaded", { clear = true }),
-    callback = function()
-        if vim.g.colors_name == "rose-pine" then
-            setup_rose_pine()
-            return
-        end
-        if vim.g.colors_name == "vague" then return end
-        require("q3yi.patch_theme").patch()
-    end,
-})
-
-vim.cmd.colorscheme("default")
+vim.cmd.colorscheme("ef-theme")
